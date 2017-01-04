05277d7c7fd42ea988a370cca2738a72c0082a05
Arif Islam <arif@dreamfactory.com>
2017-01-03 23:41:08 -0500

    updates

---
+++ src/Workspace.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace a15lam\Jarvis;
+
+class Workspace extends \a15lam\Workspace\Workspace
+{
+    protected static $configInfo = __DIR__ . '/../config.php';
+
+    protected static $logPath = __DIR__ . '/../storage/logs/';
+}
