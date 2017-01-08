c3071b4312556fc29f61b6950a8ce11f1ae721de
Arif Islam <arif@dreamfactory.com>
2017-01-07 22:12:28 -0500

    adding systemd serivce file

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 return [
-    'debug'     => false,
-    'timezone'  => 'America/New_York',
-    'latitude'  => 34.1939770,      //North
-    'longitude' => -84.2247560,     //West
-    'zenith'    => 90 + (50 / 60),  // True sunrise/sunset
-    'rule_path' => __DIR__ . DIRECTORY_SEPARATOR . 'rule.json'
+    'debug'        => false,
+    'timezone'     => 'America/New_York',
+    'latitude'     => 34.1939770,      //North
+    'longitude'    => -84.2247560,     //West
+    'zenith'       => 90 + (50 / 60),  // True sunrise/sunset
+    'rule_path'    => __DIR__ . DIRECTORY_SEPARATOR . 'rule.json',
+    'run_interval' => 3 // seconds;
 ];
--- daemon.php
+++ daemon.php
@@ -4,6 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set(\a15lam\Jarvis\Workspace::config()->get('timezone'));
 $debug = \a15lam\Jarvis\Workspace::config()->get('debug', false);
 $refresh = (isset($argv[1])) ? $argv[1] : false;
+$int = \a15lam\Jarvis\Workspace::config()->get('run_interval', 3);
 
 $engine = new \a15lam\Jarvis\Engine($refresh);
 
@@ -14,7 +15,7 @@ while(true) {
         echo "NOW: " . date('Y-m-d H:i:s', time()) . PHP_EOL;
     }
 
-    sleep(3);
+    sleep($int);
 }
 
 //print_r($engine->dumpRules());
